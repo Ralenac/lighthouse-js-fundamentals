3ca96bd51d4807f90937460fbb3dfc94f4eade5a
Ralenac <t.ralenac@gmail.com>
2021-08-22 23:57:20 +0000

    Use for loop to make list of ingredients for Banana Bread

--- ingredients.js
+++ ingredients.js
@@ -1,14 +1,11 @@
 const ingredients = ["eggs", "milk", "flour", "sugar", "baking soda", "baking powder", "chocolate chips", "bananas"];
 
-console.log('Ingredients for Banana Bread')
-let i = 0;
-while (i < ingredients.length) {
-  console.log(ingredients[i]);
-  i++ ;
-}
-
-
 
 // Write a for loop that prints out the contents of ingredients:
+console.log('Ingredients for Banana Bread');
+for (i = 0; i < ingredients.length; i++) {
+  console.log(ingredients[i])
+}
+
 
 // Write any loop (while or for) that prints out the contents of ingredients backwards:
