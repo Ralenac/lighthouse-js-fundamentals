ed0aab45622796140c29292af096f4132438b93b
Ralenac <t.ralenac@gmail.com>
2021-08-23 00:07:09 +0000

    Use for loop to make a list of ingredients backwards

--- ingredients.js
+++ ingredients.js
@@ -1,11 +1,5 @@
 const ingredients = ["eggs", "milk", "flour", "sugar", "baking soda", "baking powder", "chocolate chips", "bananas"];
-
-
-// Write a for loop that prints out the contents of ingredients:
-console.log('Ingredients for Banana Bread');
-for (i = 0; i < ingredients.length; i++) {
-  console.log(ingredients[i])
+ 
+for (var i = ingredients.length - 1; i >= 0; i--) {
+    console.log(ingredients[i]);
 }
-
-
-// Write any loop (while or for) that prints out the contents of ingredients backwards:
