341f009c5d905eca586dc180f8aec546e22ca796
Ralenac <t.ralenac@gmail.com>
2021-08-23 00:12:10 +0000

    Use for loop to make a list of ingredients backwards

--- ingredients.js
+++ ingredients.js
@@ -1,5 +1,8 @@
 const ingredients = ["eggs", "milk", "flour", "sugar", "baking soda", "baking powder", "chocolate chips", "bananas"];
  
-for (var i = ingredients.length - 1; i >= 0; i--) {
-    console.log(ingredients[i]);
+console.log('List of ingredients for Banana Bread')
+let i = 0;
+while (i < ingredients.length) {
+  console.log(ingredients[i]);
+  i++ ;
 }
